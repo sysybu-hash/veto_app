ea7696bbfc74f7b3165b267c093164ade81e3f3a
yohanan.bukshpan <sysybu@gmail.com>
2026-04-28 19:17:25 +0300

    docs: add VETO_System_Spec.md

---
+++ FINAL_NUCLEAR.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\User\Desktop\VETO_App"
+git update-ref refs/heads/main 7eb19d2
+git push origin main -f
+pause
---
+++ RESET_AND_PUSH.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\User\Desktop\VETO_App"
+git branch -f main 7eb19d2
+git push origin main -f
+pause
